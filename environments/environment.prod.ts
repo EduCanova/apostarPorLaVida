3b5329642533ccc29543c25a96929aa001696ac4
EduCanova <36701249+EduCanova@users.noreply.github.com>
2019-11-20 09:33:41 -0300

    Add files via upload

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: true
 };
 export const firebaseConfig = {
-  apiKey: "AIzaSyDW_WHXSOnIsD8HGGY_Gr_vcyZ-N5vWwOM",
-  authDomain: "chatonline-5da64.firebaseapp.com",
-  databaseURL: "https://chatonline-5da64.firebaseio.com",
-  projectId: "chatonline-5da64",
-  storageBucket: "chatonline-5da64.appspot.com",
-  messagingSenderId: "1086088481470",
-  appId: "1:1086088481470:web:5e7cdd0d67c5fa24376cef",
-  measurementId: "G-C7VSRC03P0"
+  apiKey: "AIzaSyDPZPtl7mAv_CNpn6ElH2noPlR0FR1uVrE",
+  authDomain: "apostar-por-la-vida.firebaseapp.com",
+  databaseURL: "https://apostar-por-la-vida.firebaseio.com",
+  projectId: "apostar-por-la-vida",
+  storageBucket: "apostar-por-la-vida.appspot.com",
+  messagingSenderId: "210569879066",
+  appId: "1:210569879066:web:198dee551fc35f00533ba3",
+  measurementId: "G-5M72SQ86H0"
 };
